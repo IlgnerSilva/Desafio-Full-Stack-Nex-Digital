8375c7122a378d6797b2abc2ec460e4bb9ed06cc
IlgnerSilva <ilgnersilva@outlook.com>
2022-06-15 22:13:23 -0300

    Corrigindo erro redis no windows

--- backend/redis/blacklist.js
+++ backend/redis/blacklist.js
@@ -1,2 +1,11 @@
-const redis = require('redis');
-module.exports = redis.createClient({predfix: 'blacklist', port: 6379 });
+const redis = require("redis");
+const redisUrl = process.env.REDIS_TLS_URL ? process.env.REDIS_TLS_URL : process.env.REDIS_URL;
+const redisDefaults = {
+  tls: {
+  // Heroku uses self-signed certificate, which will cause error in connection, unless check is disabled
+  rejectUnauthorized: false,
+  },
+};
+const defaultClient = redis.createClient(redisUrl, redisDefaults);
+
+module.exports = defaultClient;
